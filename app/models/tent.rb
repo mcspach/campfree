413cef88b07b9619588faf58dcf78173c7c2ee36
Andrew Bilski <andrewyangbilski@gmail.com>
2020-11-16 08:16:20 -0600

    created controllers for users, tents and bookings

--- app/models/tent.rb
+++ app/models/tent.rb
@@ -1,3 +1,11 @@
 class Tent < ApplicationRecord
   belongs_to :user
+
+  has_many :bookings, dependent: :destroy
+
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+  validates :location, presence: true
+
 end
