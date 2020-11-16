0d681b0ec149868682b61eb71e218dbd1e81c5ed
Andrew Bilski <andrewyangbilski@gmail.com>
2020-11-16 08:50:18 -0600

    Defined methods for users, tents and bookings controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,43 @@
 class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    
+    if @user.save
+      redirect_to user_path(@user), notice: "You have successfully created your profile!!"
+    else
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user)
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to root_path, notice:"Thank you for using Outpost fucker!"
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :encrypted_password, :username)
+  end
+
 end
