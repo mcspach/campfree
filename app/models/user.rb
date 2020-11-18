97fbdca48c05f18bf8a9d2df643817fade4bd898
Andrew Bilski <andrewyangbilski@gmail.com>
2020-11-17 20:02:29 -0600

    Seeded 7 tents and 3 users

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   has_many :bookings
-  has_many :tents
+  has_many :tents, dependent: :destroy
 
   validates :username, presence: true, uniqueness: true
 end
