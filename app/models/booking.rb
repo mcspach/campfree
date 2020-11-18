ac74bc3f9693e9e11e99a4055475ff40f0dae22c
Matthew Spach <matthewspach@gmail.com>
2020-11-18 11:38:07 -0600

    validations and updated devise

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :tent
+
+  validates :checkin_date, :checkout_date, presence: true
 end
--- app/models/tent.rb
+++ app/models/tent.rb
@@ -3,7 +3,7 @@ class Tent < ApplicationRecord
 
   has_many :bookings, dependent: :destroy
 
-  validates :title, presence: true
+  validates :title, presence: true, uniqueness: true
   validates :description, presence: true
   validates :price, presence: true
   validates :location, presence: true
