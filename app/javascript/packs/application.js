e3d57249a6656c909a88ad0973663426057ffb27
Andrew Bilski <andrewyangbilski@gmail.com>
2020-11-19 12:55:23 -0600

    Added mapbox-gl

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,3 +1,6 @@
+import 'mapbox-gl/dist/mapbox-gl.css';
+import { initMapbox } from '../plugins/init_mapbox';
+
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
@@ -8,6 +11,9 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+document.addEventListener('turbolinks:load', () => {
+  initMapbox();
+})
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
