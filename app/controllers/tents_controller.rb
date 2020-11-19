0e5e74f4eebedbcf28470032fbceea8e70e82a2f
Andrew Bilski <andrewyangbilski@gmail.com>
2020-11-19 15:53:26 -0600

    search filter

--- app/controllers/tents_controller.rb
+++ app/controllers/tents_controller.rb
@@ -2,7 +2,11 @@ class TentsController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :index ]
 
   def index
-    @tents = Tent.all
+    if params[:state].present? 
+      @tents = Tent.where("state ILIKE ? ", params[:state])
+    else
+      @tents = Tent.all
+    end
   end
 
   def show
