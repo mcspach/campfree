fd5bfb18c90eacbcaab72d762826aeb5d3690325
Darien Hernandez <darienhz89@gmail.com>
2020-11-19 12:26:26 -0600

    added cloudinary so that user can upload photo of their tent

--- app/models/tent.rb
+++ app/models/tent.rb
@@ -1,11 +1,10 @@
 class Tent < ApplicationRecord
   belongs_to :user
-
+  has_many_attached :photos
   has_many :bookings, dependent: :destroy
 
   validates :title, presence: true, uniqueness: true
   validates :description, presence: true
   validates :price, presence: true
   validates :location, presence: true
-
 end
