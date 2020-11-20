4d20353cfd87a53e4b3da1256a4c30e90ede9012
Andrew Bilski <andrewyangbilski@gmail.com>
2020-11-20 12:22:06 -0600

    Daniel's changes

--- app/controllers/tents_controller.rb
+++ app/controllers/tents_controller.rb
@@ -45,7 +45,7 @@ class TentsController < ApplicationController
   def update
     @tent = Tent.find(params[:id])
     @tent.update(tent_params)
-    redirect_to tent_path(current_user)
+    redirect_to tent_path(@tent)
   end
 
   def destroy
