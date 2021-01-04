6f4a987a73a98dbc625f09c06dd404d8e0dcbf25
Matthew Spach <matthewspach@gmail.com>
2021-01-04 15:09:05 -0800

    quick fixes

--- app/javascript/components/hero.js
+++ app/javascript/components/hero.js
@@ -3,9 +3,9 @@ import Typed from 'typed.js';
 const loadDynamicBannerText = () => {
   if (document.getElementById('banner-typed-text')) {
   new Typed('#banner-typed-text', {
-    strings: ["Welcome to CAMPFREE...", "Find your perfect camping getaway...", "Share info about free public land...", "Host campers on your private land..."],
-    typeSpeed: 30,
-    backDelay: 2000,
+    strings: ["Welcome to DirtBag Map...", "Find your perfect camping getaway...", "Share info about free public land...", "Host campers on your private land..."],
+    typeSpeed: 24,
+    backDelay: 3000,
     loop: true
   });}
 }
